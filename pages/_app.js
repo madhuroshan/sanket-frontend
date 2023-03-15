04b251f855cdc92812073b04514573e218e6a133
madhuroshan <madhuroshan2102@gmail.com>
2023-03-15 15:57:55 +0530

    initialised

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import "styles/variables.css";
+import "styles/globals.css";
+import "styles/text-styles.css";
+import "styles/layout.css";
+import "styles/form.css";
+
+import "assets/fonts/space-grotesk/space-grotesk.css";
+import "assets/fonts/sweet-sans-pro/sweet-sans-pro.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+// import { Inter } from "next/font/google";
+
+import Navbar from "../components/Navbar";
+// import "../styles/globalscss";
+// const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      {/* <div>Sanket'23</div> */}
+      <Navbar />
+    </>
+  );
+}

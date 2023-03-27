0b1f669283af2096ca8d20f4327e6d2e89a72c10
madhuroshan <madhuroshan2102@gmail.com>
2023-03-27 23:12:19 +0530

    updated workshops

--- components/events/Workshops.js
+++ components/events/Workshops.js
@@ -10,12 +10,13 @@ export default function Workshops() {
         <div className={styles["underline"]}></div>
         <div className={styles["events"]}>
           <EventCard
-            eventName="Quiz and Circuit Making"
+            eventName="RIG Workshop"
             eventCount="1"
-            eventDesc="Get ready to spark your creativity and unleash your electronics wizardry with our electrifying competition! With a quiz round to test your knowledge and a hardware section to showcase your skills... Get yourself prepared to design and demonstrate your circuit, impress the judges, and win amazing prizes..."
+            eventDesc="We are delighted to announce an upcoming workshop hosted by the esteemed Robotics Interest Group at NIT Calicut, as a part of Sanket'23. The workshop is designed to provide comprehensive insights into an array of topics related to robotics and mechatronics, ranging from SolidWorks, electronics, Proteus, control systems and programming to neural networks. It is a unique opportunity to enrich your knowledge of robotics and mechatronics and gain hands-on experience from the industry experts. We strongly encourage you to register for this informative workshop to enhance your proficiency in these cutting-edge technologies"
             left="left"
             linknit="/events/workshops/workshop1"
             linknonnit="/events/workshops/workshop1"
+            date="Time &nbsp;:  &nbsp;Apr 1st &nbsp;, 9am - 12pm & 2pm - 5pm"
           />
 
           <EventCard
